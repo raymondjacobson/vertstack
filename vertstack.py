80fd41994e6ae5293576bd67715c81590230853e
Raymond Jacobson <raymondshujacobson8@gmail.com>
2013-07-06 21:20:19 -0400

    add to .gitignore

--- vertstack.py
+++ vertstack.py
@@ -4,20 +4,20 @@
 
     A simple app for generating your own timeline
 
-    Keywords: Flask, sqlite, Isotope
+    Keywords: Flask, sqlite, SASS, CoffeeScript
 
     :copyright: (c) 2013 Raymond Jacobson
 
 """
-from flask import (
-    Flask,
-    render_template,
-)
+from flask import Flask, render_template
+
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
 def index():
-    return render_template('vertstack.html')
+    return render_template('stack.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
